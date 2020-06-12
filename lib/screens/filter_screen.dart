e514bcadbfaf1aec504aa1413a1c806dfcbaa23e
Santosh Mainali <cyx977@outlook.com>
2020-06-12 10:01:29 +0545

    SwitchListTile for filter

--- lib/screens/filter_screen.dart
+++ lib/screens/filter_screen.dart
@@ -8,11 +8,66 @@ class FiltersScreen extends StatefulWidget {
 }
 
 class _FiltersScreenState extends State<FiltersScreen> {
+  var isGlutenFree = false;
+  var isVegan = false;
+  var isVegeterian = false;
+  var isLactoseFree = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: MainDrawer(),
       appBar: AppBar(),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text("Adjust your meal selection",
+                style: Theme.of(context).textTheme.headline6),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                SwitchListTile(
+                  title: Text("Gluten Free"),
+                  value: isGlutenFree,
+                  onChanged: (bool val) {
+                    setState(() {
+                      isGlutenFree = val;
+                    });
+                  },
+                ),
+                SwitchListTile(
+                  title: Text("Vegan"),
+                  value: isVegan,
+                  onChanged: (bool val) {
+                    setState(() {
+                      isVegan = val;
+                    });
+                  },
+                ),
+                SwitchListTile(
+                  title: Text("Vegeterian"),
+                  value: isVegeterian,
+                  onChanged: (bool val) {
+                    setState(() {
+                      isVegeterian = val;
+                    });
+                  },
+                ),
+                SwitchListTile(
+                  title: Text("Lactose Free"),
+                  value: isLactoseFree,
+                  onChanged: (bool val) {
+                    setState(() {
+                      isLactoseFree = val;
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
